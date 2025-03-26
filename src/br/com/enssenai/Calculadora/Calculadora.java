99ea13e36fdff5f100019f85a32ced2257814041
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-03-26 08:27:54 -0300

    Criando Classe Menu

--- src/br/com/enssenai/Calculadora/Calculadora.java
+++ src/br/com/enssenai/Calculadora/Calculadora.java
@@ -2,37 +2,33 @@
 
 package br.com.enssenai.Calculadora;
 
-import br.com.enssenai.Calculadora.model.Circunferencia;
-import br.com.enssenai.Calculadora.model.Quadrado;
-import br.com.enssenai.Calculadora.model.Retangulo;
-import br.com.enssenai.Calculadora.model.Trapezio;
-import br.com.enssenai.Calculadora.model.Triangulo;
+import br.com.enssenai.Calculadora.model.*;
 
 public class Calculadora {
 
 	public static void main(String[] args) {
 		
-		Quadrado quadrado = new Quadrado();
-		quadrado.calcularArea();
-		quadrado.calcularPerimetro();
-		quadrado.mostrarDados();
-		
-		Retangulo retangulo = new Retangulo();
-		retangulo.calcularArea();
-		retangulo.calcularPerimetro();
-		retangulo.mostrarDados();
-		
-		Triangulo triangulo = new Triangulo();
-		triangulo.calcularArea();
-		triangulo.mostrarDados();
-		
-		Trapezio trapezio = new Trapezio();
-		trapezio.calcularArea();
-		trapezio.mostrarDados();
-		
-		Circunferencia circunferencia = new Circunferencia();
-		circunferencia.calcularArea();
-		circunferencia.mostrarDados();
+//		Quadrado quadrado = new Quadrado();
+//		quadrado.calcularArea();
+//		quadrado.calcularPerimetro();
+//		quadrado.mostrarDados();
+//		
+//		Retangulo retangulo = new Retangulo();
+//		retangulo.calcularArea();
+//		retangulo.calcularPerimetro();
+//		retangulo.mostrarDados();
+//		
+//		Triangulo triangulo = new Triangulo();
+//		triangulo.calcularArea();
+//		triangulo.mostrarDados();
+//		
+//		Trapezio trapezio = new Trapezio();
+//		trapezio.calcularArea();
+//		trapezio.mostrarDados();
+//		
+//		Circunferencia circunferencia = new Circunferencia();
+//		circunferencia.calcularArea();
+//		circunferencia.mostrarDados();
 		
 		
 	}
---
+++ src/br/com/enssenai/Calculadora/ui/Menu.java
@@ -0,0 +1,9 @@
+package br.com.enssenai.Calculadora.ui;
+
+import java.util.Scanner;
+
+public class Menu {
+	
+	Scanner reader = new Scanner(System.in);
+
+}
